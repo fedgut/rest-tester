1b2936e8cd9bd8ab86fd2afbf7de5f4de04d23d4
fedgut <fedgut@gmail.com>
2019-10-25 12:05:31 -0500

    Add scripts for GET

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -3,3 +3,11 @@ require 'rest-client'
 url = 'http://localhost:3000/users'
 
 puts RestClient.get(url)
+
+puts RestClient.get(url + '/new')
+
+puts RestClient.get(url + '/1')
+
+puts RestClient.get(url + '1/edit')
+
+puts RestClient.post(url, person: 'one')
